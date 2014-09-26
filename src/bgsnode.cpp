6a1d332f039990c824c3d15c0276e18bd2a6f5d7
Garrett Brown <garbearucla@gmail.com>
2014-09-26 10:57:36 -0700

    Update node

--- src/bgsnode.cpp
+++ src/bgsnode.cpp
@@ -21,6 +21,48 @@
  */
 
 #include "package_bgs/FrameDifferenceBGS.h"
+#include "package_bgs/StaticFrameDifferenceBGS.h"
+#include "package_bgs/WeightedMovingMeanBGS.h"
+#include "package_bgs/WeightedMovingVarianceBGS.h"
+#include "package_bgs/MixtureOfGaussianV1BGS.h"
+#include "package_bgs/MixtureOfGaussianV2BGS.h"
+#include "package_bgs/AdaptiveBackgroundLearning.h"
+#include "package_bgs/AdaptiveSelectiveBackgroundLearning.h"
+
+#if CV_MAJOR_VERSION >= 2 && CV_MINOR_VERSION >= 4 && CV_SUBMINOR_VERSION >= 3
+#include "package_bgs/GMG.h"
+#endif
+
+#include "package_bgs/dp/DPAdaptiveMedianBGS.h"
+#include "package_bgs/dp/DPGrimsonGMMBGS.h"
+#include "package_bgs/dp/DPZivkovicAGMMBGS.h"
+#include "package_bgs/dp/DPMeanBGS.h"
+#include "package_bgs/dp/DPWrenGABGS.h"
+#include "package_bgs/dp/DPPratiMediodBGS.h"
+#include "package_bgs/dp/DPEigenbackgroundBGS.h"
+#include "package_bgs/dp/DPTextureBGS.h"
+#include "package_bgs/tb/T2FGMM_UM.h"
+#include "package_bgs/tb/T2FGMM_UV.h"
+#include "package_bgs/tb/T2FMRF_UM.h"
+#include "package_bgs/tb/T2FMRF_UV.h"
+#include "package_bgs/tb/FuzzySugenoIntegral.h"
+#include "package_bgs/tb/FuzzyChoquetIntegral.h"
+#include "package_bgs/lb/LBSimpleGaussian.h"
+#include "package_bgs/lb/LBFuzzyGaussian.h"
+#include "package_bgs/lb/LBMixtureOfGaussians.h"
+#include "package_bgs/lb/LBAdaptiveSOM.h"
+#include "package_bgs/lb/LBFuzzyAdaptiveSOM.h"
+#include "package_bgs/ck/LbpMrf.h"
+#include "package_bgs/jmo/MultiLayerBGS.h"
+// The PBAS algorithm was removed from BGSLibrary because it is
+// based on patented algorithm ViBE
+// http://www2.ulg.ac.be/telecom/research/vibe/
+//#include "package_bgs/pt/PixelBasedAdaptiveSegmenter.h"
+#include "package_bgs/av/VuMeter.h"
+#include "package_bgs/ae/KDE.h"
+#include "package_bgs/db/IndependentMultimodalBGS.h"
+#include "package_bgs/sjn/SJN_MultiCueBGS.h"
+#include "package_bgs/bl/SigmaDeltaBGS.h"
 
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
@@ -31,17 +73,32 @@
 
 #define NODE_NAME  "bgsnode"
 
+// MixtureOfGaussianV2BGS
+// AdaptiveBackgroundLearning
+// AdaptiveSelectiveBackgroundLearning ***
+// DPGrimsonGMMBGS
+// DPZivkovicAGMMBGS
+// DPEigenbackgroundBGS ***
+// T2FGMM_UV
+// FuzzySugenoIntegral
+// FuzzyChoquetIntegral
+// MultiLayerBGS
+// IndependentMultimodalBGS
+// SigmaDeltaBGS
+
+
 class BackgroundModeller
 {
 public:
   BackgroundModeller(void)
    : m_imgTransport(m_nodeHandle),
-     m_bgsPackage(new FrameDifferenceBGS)
+     m_bgsPackage(new SigmaDeltaBGS)
   {
     m_imgSubscriber = m_imgTransport.subscribe("/v4l/camera/image_raw", 1,
       &BackgroundModeller::ReceiveImage, this, image_transport::TransportHints("compressed"));
 
-    m_imgPublisher = m_imgTransport.advertise("/image_processor/output_video", 1);
+    m_imgPublisherForeground = m_imgTransport.advertise("/bgs/foreground", 1);
+    m_imgPublisherBackground = m_imgTransport.advertise("/bgs/background", 1);
   }
 
   ~BackgroundModeller(void)
@@ -66,19 +123,29 @@ public:
     cv::Mat img_bkgmodel;
     m_bgsPackage->process(cv_ptr->image, img_mask, img_bkgmodel);
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255 ,0, 0));
+    if (!img_mask.empty())
+    {
+      if (!img_bkgmodel.empty())
+      {
+        img_bkgmodel.copyTo(cv_ptr->image);
+        m_imgPublisherBackground.publish(cv_ptr->toImageMsg());
+      }
 
-    // Output modified video stream
-    m_imgPublisher.publish(cv_ptr->toImageMsg());
+      if (!img_mask.empty())
+      {
+        img_mask.copyTo(cv_ptr->image);
+        cv_ptr->encoding = "mono8";
+        m_imgPublisherForeground.publish(cv_ptr->toImageMsg());
+      }
+    }
   }
 
 private:
   ros::NodeHandle m_nodeHandle;
   image_transport::ImageTransport m_imgTransport;
   image_transport::Subscriber m_imgSubscriber;
-  image_transport::Publisher m_imgPublisher;
+  image_transport::Publisher m_imgPublisherForeground;
+  image_transport::Publisher m_imgPublisherBackground;
   IBGS* m_bgsPackage;
 };
 
